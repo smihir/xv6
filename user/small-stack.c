0684f9aeb6ac8f1659cfa50871cbdd4fc7207d73
Mihir Shete <mihirsht@gmail.com>
2015-10-24 20:31:37 -0500

    add test to create some variables on stack and print them

--- user/small-stack.c
+++ user/small-stack.c
@@ -1,9 +1,28 @@
 #include "types.h"
 #include "user.h"
+#include "param.h"
 
+#define N 10
 int
 main(int argc, char *argv[])
 {
-  printf(1, "%s", "placeholder");
+  int a[N];
+  int b[N]; //b will have addr less than a as stack grows upward
+  int i;
+
+  printf(1, "USERTOP is at %d, stack should start from %d\n",
+             USERTOP, USERTOP - 4096);
+
+  for(i = 0; i < N; i++){
+      a[i] = i;
+      b[i] = i;
+  }
+  for(i = 0; i < N; i++){
+      printf(1, "addr of a[%d] -> %d, value %d\n", i, &a[i], a[i]);
+  }
+  printf(1, "\n");
+  for(i = 0; i < N; i++){
+      printf(1, "addr of b[%d] -> %d, value %d\n", i, &b[i], b[i]);
+  }
   exit();
 }
