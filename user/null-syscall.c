2ff047bc2a984cd769708ce735cacba038a37fcd
Mihir Shete <mihirsht@gmail.com>
2015-10-24 19:48:16 -0500

    create test to pass NULL pointer in a syscall

--- user/null-syscall.c
+++ user/null-syscall.c
@@ -1,9 +1,28 @@
 #include "types.h"
 #include "user.h"
+#include "fcntl.h"
 
 int
 main(int argc, char *argv[])
 {
-  printf(1, "%s", "placeholder");
+  int fd;
+  char f[80], *fnull = NULL;
+
+  fd = open("README", O_RDONLY);
+
+  if (fd == -1){
+    printf(1, "error opening file\n");
+    exit();
+  }
+
+  if (read(fd, f, sizeof(f)) == -1){
+    printf(1, "error reading file into f\n");
+    exit();
+  }
+
+  if (read(fd, fnull, 80) == -1){
+    printf(1, "error reading file into fnull\n");
+    exit();
+  }
   exit();
 }
