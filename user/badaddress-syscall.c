08b935909bd95d7b0a824cabf2241c7527debe16
Mihir Shete <mihirsht@gmail.com>
2015-10-24 20:31:06 -0500

    add test to pass addres between heap and stack to a syscall

--- user/badaddress-syscall.c
+++ user/badaddress-syscall.c
@@ -1,9 +1,34 @@
 #include "types.h"
 #include "user.h"
+#include "fcntl.h"
+#include "param.h"
 
+// bytes mapped by a page
+#define PGSIZE 4096
 int
 main(int argc, char *argv[])
 {
-  printf(1, "%s", "placeholder");
+  int fd;
+  char f[80];
+  // some address between stack and heap
+  char *fnull = (char *)(USERTOP - PGSIZE - sizeof(f));
+
+  fd = open("README", O_RDONLY);
+
+  printf(1, "addr of f: %d addt of fnull: %d\n", f, fnull);
+  if (fd == -1){
+    printf(1, "error opening file\n");
+    exit();
+  }
+
+  if (read(fd, f, sizeof(f)) == -1){
+    printf(1, "error reading file into f\n");
+    exit();
+  }
+
+  if (read(fd, fnull, 80) == -1){
+    printf(1, "error reading file into fnull\n");
+    exit();
+  }
   exit();
 }
