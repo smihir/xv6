bd784895240f7a45523eefe9f032dc08d6e8b6f3
rashij <rashi.bits@gmail.com>
2015-11-07 13:02:53 -0600

    user thread library functions for thread create and join

--- user/tlib.c
+++ user/tlib.c
@@ -1,16 +1,26 @@
 #include "types.h"
 #include "user.h"
 
+#define PGSIZE		4096
+
 int
 thread_create(void (*start_routine)(void*), void *arg)
 {
-	return 0;
+	void *tmp, *stack;
+	if((tmp = malloc(2 * PGSIZE)) == 0)
+		return -1;
+	//TODO: optimize this to free all the allocated space
+	stack = (void *)((uint)tmp + (PGSIZE - (uint)tmp % PGSIZE));
+	return clone(start_routine, arg, stack);
 }
 
 int
 thread_join()
 {
-	return 0;
+	void *stack;
+	int ret = join(&stack);
+	free(stack);
+	return ret;
 }
 
 void
